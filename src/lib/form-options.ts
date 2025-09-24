47e2871bcfde83605cf4dbf716835c706f18e028
Juan José Rodríguez <jjrodriguez@lksnext.com>
2025-09-24 17:39:25 +0000

    fix key options
    Signed-off-by: Juan José Rodríguez <jjrodriguez@lksnext.com>

--- src/lib/form-options.ts
+++ src/lib/form-options.ts
@@ -18,6 +18,7 @@ export const RSA_KEY_SIZE_OPTIONS = [
 ];
 
 export const ECDSA_CURVE_OPTIONS = [
+  { value: 'P-224', label: 'P-224 (NIST P-224, secp224r1)' },
   { value: 'P-256', label: 'P-256 (NIST P-256, secp256r1)' },
   { value: 'P-384', label: 'P-384 (NIST P-384, secp384r1)' },
   { value: 'P-521', label: 'P-521 (NIST P-521, secp521r1)' },
