adc2973a500cecdfe7b50aed930f0eb5d3a17f90
Mikel Amuchastegui Zubizarreta <mamuchastegui@lksnext.com>
2026-09-24 16:36:16 +0200

    refactor: change switch by an if to make it more readable
    Signed-off-by: Mikel Amuchastegui Zubizarreta <mamuchastegui@lksnext.com>

--- src/components/shared/IntegrationIcon.tsx
+++ src/components/shared/IntegrationIcon.tsx
@@ -6,10 +6,9 @@ import type { DiscoveredIntegration } from '@/lib/integrations-api';
 import AwsIcon from '@/app/aws.svg';
 
 export const IntegrationIcon: React.FC<{ type: DiscoveredIntegration['type'] }> = ({ type }) => {
-  switch (type) {
-    case 'AWS_IOT_CORE':
-      return <Image src={AwsIcon} alt="AWS IoT Core Icon" className="h-6 w-6" width={24} height={24} />;
-    default:
-      return <Blocks className="h-6 w-6 text-primary" />;
+  if (type === 'AWS_IOT_CORE') {
+    return <Image src={AwsIcon} alt="AWS IoT Core Icon" className="h-6 w-6" width={24} height={24} />;
   }
+
+  return <Blocks className="h-6 w-6 text-primary" />;
 };
